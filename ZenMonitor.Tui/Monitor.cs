a78a10813d6c01ab8140beae8a3b6184b305a21f
Akeoot <akeoot@pm.me>
2026-05-11 11:37:48 +0200

    feat(tui): add MainWindow and Monitor stub classes for TUI implementation

---
+++ ZenMonitor.Tui/Monitor.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Ame (Akeoot/Akeoott) <akeoot@pm.me>. Licensed under the LGPL-3.0 Licence.
+// See the LICENSE file in the repository root for full license text.
+
+using Microsoft.Extensions.Logging;
+
+using Terminal.Gui.App;
+using Terminal.Gui.Configuration;
+using Terminal.Gui.Input;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+using ZenMonitor.Core.Interfaces;
+using ZenMonitor.Core.Models;
+
+namespace ZenMonitor.Tui;
+
+public class Monitor(
+    ILogger<Monitor> logger,
+    ICpu cpuInfo,
+    IGpu gpuInfo,
+    IMemory memoryInfo,
+    INetwork networkInfo,
+    IDrive driveInfo,
+    ISystem systemInfo)
+{
+    private readonly ILogger<Monitor> _logger = logger;
+    private readonly ICpu _cpuInfo = cpuInfo;
+    private readonly IGpu _gpuInfo = gpuInfo;
+    private readonly IMemory _memoryInfo = memoryInfo;
+    private readonly INetwork _networkInfo = networkInfo;
+    private readonly IDrive _driveInfo = driveInfo;
+    private readonly ISystem _systemInfo = systemInfo;
+
+    private readonly SemaphoreSlim _dataReadyEvent = new(0, int.MaxValue);
+
+    public async Task InitMonitor(int loopDelay, CancellationToken cts)
+    {
+
+    }
+}

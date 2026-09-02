63be4cbe1dcd35efbc2fb26743231ed2c0b596f3
Akeoot <akeoot@pm.me>
2026-09-03 01:33:27 +0200

    fix(tauri): set environment vars for linux to ensure compatability on wayland

--- src/src-tauri/src/main.rs
+++ src/src-tauri/src/main.rs
@@ -4,5 +4,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 fn main() {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        // Doesn't work on wayland so gotta add these var's...
+        std::env::set_var("GDK_BACKEND", "x11");
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
     zenmonitor_lib::run()
 }
